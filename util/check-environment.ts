4fcb3b352677e452838bb1d3adc371805816221b
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-06-06 21:52:08 +0400

    Change url

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -1,8 +1,8 @@
 export default function checkEnvironment(): string {
   const envUrl =
     process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://trello-clone-one.vercel.app';
+      ? 'https://dash.agileflow.tech'
+      : 'https://dash.agileflow.tech';
 
   return envUrl;
 }
